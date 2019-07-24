6a83f2847822a08d4c75217abebaa59f817f183b
george21-meet <george21@meet.mit.edu>
2019-07-24 07:21:02 -0400

    Stopping here

---
+++ lab4pt2.py
@@ -0,0 +1,19 @@
+#fruit=input("what kind of fruit am i sorting" )
+#if fruit=="apple":
+#    print("apples in bin 1")
+#elif fruit=="banana":
+#    print("bananas in bin 3")
+#elif fruit=="orange":
+#    print("oranges in bin 2")
+#else:
+#    print("i dont recognize that fruit")
+num1 = 2
+num2 = 3
+num3 = 1
+
+if num1 > num3:
+	print('chocolate')
+elif num1 > num2:
+	print('vanilla')
+else:
+    print('strawberry')
